a236443dffbc786d7b426d6e04866fc4b0e005d4
Ryan Dunn <contact@ryandunn.dev>
2024-04-29 16:22:40 +0100

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { pixelify } from "./fonts";
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Ryan Dunn - Developer",
@@ -14,6 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/png"
+          sizes="32x32"
+        />
+      </Head>
       <body className={`${pixelify.className} line-bg-med`}>{children}</body>
     </html>
   );
